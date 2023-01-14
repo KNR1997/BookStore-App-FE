e8d0fc281f88f9e44032b18d03555eb0c4d94dc3
kethaka <kethakaranasinghe@gmail.com>
2023-01-14 13:56:23 +0530

    HomePage created

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,25 @@
 import React from 'react';
 import './App.css';
+import Carousel from './layouts/NavbarAndFooter/HomePage/Carousel';
+import ExploreTopBooks from './layouts/NavbarAndFooter/HomePage/ExploreTopBooks';
+import Footer from './layouts/NavbarAndFooter/HomePage/Footer';
+import Heros from './layouts/NavbarAndFooter/HomePage/Heros';
+import LibraryServices from './layouts/NavbarAndFooter/HomePage/LibraryServices';
+import ReturnBook from './layouts/NavbarAndFooter/HomePage/ReturnBook';
+import Navbar from './layouts/NavbarAndFooter/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Welcome kethaka</h1>
+    <div>
+      <Navbar />
+      <ExploreTopBooks />
+      <Carousel />
+      <ReturnBook />
+      <Heros />
+      <LibraryServices />
+      <Footer />
     </div>
+
   );
 }
 
